b9d1ca70fdd16246bfe7dc08020b6def2ed715a6
Kirill Valikov <Kirill.Valikov@infowatch.com>
2020-03-07 22:29:45 +0300

    todo basically works, needs refactoring

--- my/todo/root.view.ts
+++ my/todo/root.view.ts
@@ -2,23 +2,43 @@ namespace $.$$ {
 	export class $my_root extends $.$my_root {
 		@ $mol_mem
 		todo_items( next = [] as string[] ) { return next }
+		@ $mol_mem
+		finished_items( next = [] as string[] ) { return next }
 
 		addTodoItem(){
 			this.todo_items([...this.todo_items(), this.name()]);
 			this.name("");
 		}
+
+		getTodoTitle(){
+			return this.todo_items().length ? ['Задачи, которые надо выполнить:'] : ['']
+		}
+		
 		todo_item(id:number){
 			return this.todo_items()[id];
+		}
+
+		finished_item(id:number){
+			return this.finished_items()[id];
 		}
 		
 		root_rows(){
 			return this.todo_items().map( ( task , index )=> this.Task_row( index ) )
 		}
+		finished_rows(){
+			return this.finished_items().map( ( task , index )=> this.Finished_row( index ) )
+		}
+		restoreItem(id: number){
+			const finished_items= [...this.finished_items()];
+			const toRestore = finished_items.splice(id, 1);
+			this.finished_items(finished_items);
+			this.todo_items([...this.todo_items(), ...toRestore]);
+		}
 
 		deleteTodoItem(id: number){
-			console.log(id);
 			const items = [...this.todo_items()];
-			items.splice(id, 1);
+			const deleted = items.splice(id, 1);
+			this.finished_items([...this.finished_items(), ...deleted]);
 			this.todo_items([...items]);
 		}
 	}
